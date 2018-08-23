d8e5ab2683e95bba04c6351d8e366b9c151d6adf
Cliff McCall <cliff.mccall1@gmail.com>
2018-08-23 15:08:07 -0500

    Variables and Data Types Exercise

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!!!')
+// Variables and Data Types
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variables cannot start with a number or symbol except for $ or _
+// You cannot use JavaScript keywords as vairable names
